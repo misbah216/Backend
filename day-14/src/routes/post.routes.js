968899fc603a91035dfd322c6e700c7ecb417b9a
Misbah Saifi <saifimisbah7@gmail.com>
2026-07-29 20:53:04 +0530

    imagekit installed and uploaded

--- day-14/src/routes/post.routes.js
+++ day-14/src/routes/post.routes.js
@@ -2,8 +2,9 @@ const express = require("express")
 const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
 const multer = require("multer")
+const upload = multer({storage: multer.memoryStorage() })
 
 
-postRouter.post("/", postController.createPostController)
+postRouter.post("/", upload.single("image"), postController.createPostController)
 
 module.exports= postRouter
